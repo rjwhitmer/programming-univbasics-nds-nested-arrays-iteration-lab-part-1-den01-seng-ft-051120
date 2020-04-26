17d74093a9a216df2aeba3bde5532037aca0dd6c
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 20:42:33 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,21 @@
+require 'pry'
+
 def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
-
+  array_index = 0 
+  #binding.pry
+  while array_index < src.count do 
+    index_element = 0 
+    
+    while index_element < src[array_index].count do 
+      if src[array_index][index_element] % 2 == 0 
+        p src[array_index][index_element]
+      end
+      index_element += 1 
+    end
+    array_index += 1 
+  end
+  #binding.pry
 end
